e3f8dc6722edf545f756e3d69188941d8d058cc7
aashisoni000 <aashi2701soni@gmail.com>
2026-02-27 16:29:20 +0530

    UC2 hardcoded palindrome logic

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,8 +1,22 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
+        //UC1
         System.out.println("welcome to palindrome management system");
         System.out.println("version : 1.0");
         System.out.println("System initialized Successfully done");
 
+        String input = "madam";
+        String reversed = "";
+        // UC2
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        if (input.equals(reversed)) {
+            System.out.println("The hardcoded string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The hardcoded string \"" + input + "\" is NOT a palindrome.");
+        }
+
+        System.out.println("Program finished.");
     }
 }
